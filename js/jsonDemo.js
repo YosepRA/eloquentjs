29ec44152ac85733cfd4ae39811c0c6a7a88028f
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2019-07-19 08:32:24 +0700

    Delete excess files from restructuring folders.

--- js/jsonDemo.js
+++
@@ -1,7 +0,0 @@
-let string = JSON.stringify({
-  squirrels: false,
-  events: ['weekend']
-});
-
-console.log(typeof string);
-console.log(JSON.parse(string).events);
--- js/sumBy10.js
+++
@@ -1,7 +0,0 @@
-let result = 0;
-
-for (let i = 1; i < 11; i++) {
-  result += i;
-}
-
-console.log(result);
